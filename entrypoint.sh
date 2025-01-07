a9dfa904e7985eda284d6b9b29f73f21700f18a2
Francisco Miranda <franciscomirandaa@gmail.com>
2025-01-07 09:11:58 -0300

    Add debug logger option

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,6 +14,12 @@ fi
 
 params="$params $tests"
 
+# https://docs.github.com/en/actions/monitoring-and-troubleshooting-workflows/enabling-debug-logging
+# https://docs.github.com/en/actions/learn-github-actions/variables#default-environment-variables
+if [ "${RUNNER_DEBUG}" = "1" ]; then
+  params="${params} --log-level debug"
+fi
+
 echo running: okteto "$command" "$params"
 # shellcheck disable=SC2086
 okteto $command $params
