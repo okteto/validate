42de5511f379359b0e203895605b8573d0d84a34
Javier Lopez <javier@okteto.com>
2026-03-18 14:53:28 +0100

    Add support for custom CA certificate in entrypoint script
    Signed-off-by: Javier Lopez <javier@okteto.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,6 +17,16 @@ if [ ! -z "$log_level" ]; then
   log_level="--log-level ${log_level}"
 fi
 
+if [ ! -z "$OKTETO_CA_CERT" ]; then
+   echo "Custom certificate is provided"
+   echo "$OKTETO_CA_CERT" > /etc/ssl/certs/okteto_ca_cert.pem
+   if command -v update-ca-certificates > /dev/null 2>&1; then
+     mkdir -p /usr/local/share/ca-certificates
+     cp /etc/ssl/certs/okteto_ca_cert.pem /usr/local/share/ca-certificates/okteto_ca_cert.crt
+     update-ca-certificates
+   fi
+fi
+
 # https://docs.github.com/en/actions/monitoring-and-troubleshooting-workflows/enabling-debug-logging
 # https://docs.github.com/en/actions/learn-github-actions/variables#default-environment-variables
 if [ "${RUNNER_DEBUG}" = "1" ]; then
